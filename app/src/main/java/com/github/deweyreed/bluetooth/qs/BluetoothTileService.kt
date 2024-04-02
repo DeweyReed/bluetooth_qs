7188d41085160e368f3841b37a542f7a9df0f9ce
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2024-04-02 09:08:35 +0800

    Open the bluetooth settings from the tile if it's disabled

--- app/src/main/java/com/github/deweyreed/bluetooth/qs/BluetoothTileService.kt
+++ app/src/main/java/com/github/deweyreed/bluetooth/qs/BluetoothTileService.kt
@@ -1,7 +1,11 @@
 package com.github.deweyreed.bluetooth.qs
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
+import android.content.Intent
 import android.graphics.drawable.Icon
+import android.os.Build
+import android.provider.Settings
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
@@ -10,7 +14,29 @@ class BluetoothTileService : TileService(), BluetoothBroadcastReceiver.BlueTooth
 
     override fun onClick() {
         super.onClick()
+        if (!isBluetoothEnabled()) {
+            startActivityAndCollapseCompat(
+                Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+        }
         toggleBluetooth()
+    }
+
+    private fun startActivityAndCollapseCompat(intent: Intent) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startActivityAndCollapse(
+                PendingIntent.getActivity(
+                    this,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            )
+        } else {
+            @Suppress("DEPRECATION", "StartActivityAndCollapseDeprecated")
+            startActivityAndCollapse(intent)
+        }
     }
 
     override fun onStartListening() {
