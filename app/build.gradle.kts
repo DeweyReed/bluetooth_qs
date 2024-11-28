0347f6f028879b4defc91ea15af2de6f9baf2b91
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2024-11-28 20:54:55 +0800

    Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.github.deweyreed.bluetooth.qs"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.deweyreed.bluetooth.qs"
